71d7203ce7af11f17ea28cd212376300f243fa0f
msserpa <msserpa@inf.ufrgs.br>
2019-03-12 15:37:48 -0300

    hotspot fixes

--- openmp/hotspot/hotspot_openmp.cpp
+++ openmp/hotspot/hotspot_openmp.cpp
@@ -38,24 +38,18 @@ const FLOAT t_chip = 0.0005;
 const FLOAT chip_height = 0.016;
 const FLOAT chip_width = 0.016;
 
-#ifdef OMP_OFFLOAD
-#pragma offload_attribute(push, target(mic))
-#endif
 
 /* ambient temperature, assuming no package at all	*/
 const FLOAT amb_temp = 80.0;
-
-int num_omp_threads;
 
 /* Single iteration of the transient solver in the grid model.
  * advances the solution of the discretized difference equations 
  * by one time step
  */
 void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col,
-					  FLOAT Cap_1, FLOAT Rx_1, FLOAT Ry_1, FLOAT Rz_1, 
-					  FLOAT step)
+					  FLOAT Cap_1, FLOAT Rx_1, FLOAT Ry_1, FLOAT Rz_1)
 {
-    FLOAT delta;
+    FLOAT delta = 0;
     int r, c;
     int chunk;
     int num_chunk = row*col / (BLOCK_SIZE_R * BLOCK_SIZE_C);
@@ -63,10 +57,7 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
     int chunks_in_col = row/BLOCK_SIZE_R;
 
 #ifdef OPEN
-    #ifndef __MIC__
-	omp_set_num_threads(num_omp_threads);
-    #endif
-    #pragma omp parallel for shared(power, temp, result) private(chunk, r, c, delta) firstprivate(row, col, num_chunk, chunks_in_row) schedule(static)
+    #pragma omp parallel for shared(power, temp, result) private(chunk, r, c) firstprivate(delta,row, col, num_chunk, chunks_in_row) schedule(static)
 #endif
     for ( chunk = 0; chunk < num_chunk; ++chunk )
     {
@@ -148,9 +139,6 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
     }
 }
 
-#ifdef OMP_OFFLOAD
-#pragma offload_attribute(pop)
-#endif
 
 /* Transient solver driver routine: simply converts the heat 
  * transfer differential equations to difference equations 
@@ -182,13 +170,6 @@ void compute_tran_temp(FLOAT *result, int num_iterations, FLOAT *temp, FLOAT *po
 	fprintf(stdout, "Rx: %g\tRy: %g\tRz: %g\tCap: %g\n", Rx, Ry, Rz, Cap);
 	#endif
 
-#ifdef OMP_OFFLOAD
-        int array_size = row*col;
-#pragma omp target \
-        map(temp[0:array_size]) \
-        map(to: power[0:array_size], row, col, Cap_1, Rx_1, Ry_1, Rz_1, step, num_iterations) \
-        map( result[0:array_size])
-#endif
         {
             FLOAT* r = result;
             FLOAT* t = temp;
@@ -197,7 +178,7 @@ void compute_tran_temp(FLOAT *result, int num_iterations, FLOAT *temp, FLOAT *po
                 #ifdef VERBOSE
                 fprintf(stdout, "iteration %d\n", i++);
                 #endif
-                single_iteration(r, t, power, row, col, Cap_1, Rx_1, Ry_1, Rz_1, step);
+                single_iteration(r, t, power, row, col, Cap_1, Rx_1, Ry_1, Rz_1);
                 FLOAT* tmp = t;
                 t = r;
                 r = tmp;
@@ -208,7 +189,7 @@ void compute_tran_temp(FLOAT *result, int num_iterations, FLOAT *temp, FLOAT *po
 	#endif
 }
 
-void fatal(char *s)
+void fatal(char const *s)
 {
 	fprintf(stderr, "error: %s\n", s);
 	exit(1);
@@ -238,7 +219,7 @@ void writeoutput(FLOAT *vect, int grid_rows, int grid_cols, char *file) {
 
 void read_input(FLOAT *vect, int grid_rows, int grid_cols, char *file)
 {
-  	int i, index;
+  	int i;
 	FILE *fp;
 	char str[STR_SIZE];
 	FLOAT val;
@@ -259,34 +240,33 @@ void read_input(FLOAT *vect, int grid_rows, int grid_cols, char *file)
 	fclose(fp);	
 }
 
-void usage(int argc, char **argv)
+void usage(char **argv)
 {
 	fprintf(stderr, "Usage: %s <grid_rows> <grid_cols> <sim_time> <no. of threads><temp_file> <power_file>\n", argv[0]);
 	fprintf(stderr, "\t<grid_rows>  - number of rows in the grid (positive integer)\n");
 	fprintf(stderr, "\t<grid_cols>  - number of columns in the grid (positive integer)\n");
 	fprintf(stderr, "\t<sim_time>   - number of iterations\n");
-	fprintf(stderr, "\t<no. of threads>   - number of threads\n");
 	fprintf(stderr, "\t<temp_file>  - name of the file containing the initial temperature values of each cell\n");
 	fprintf(stderr, "\t<power_file> - name of the file containing the dissipated power values of each cell\n");
-        fprintf(stderr, "\t<output_file> - name of the output file\n");
+        // fprintf(stderr, "\t<output_file> - name of the output file\n");
 	exit(1);
 }
 
 int main(int argc, char **argv)
 {
-	int grid_rows, grid_cols, sim_time, i;
+	int grid_rows, grid_cols, sim_time;
 	FLOAT *temp, *power, *result;
-	char *tfile, *pfile, *ofile;
+	char *tfile, *pfile;
+    // , *ofile;
 	
 	/* check validity of inputs	*/
-	if (argc != 8)
-		usage(argc, argv);
+	if (argc != 6)
+		usage(argv);
 	if ((grid_rows = atoi(argv[1])) <= 0 ||
 		(grid_cols = atoi(argv[2])) <= 0 ||
-		(sim_time = atoi(argv[3])) <= 0 || 
-		(num_omp_threads = atoi(argv[4])) <= 0
+		(sim_time = atoi(argv[3])) <= 0
 		)
-		usage(argc, argv);
+		usage(argv);
 
 	/* allocate memory for the temperature and power arrays	*/
 	temp = (FLOAT *) calloc (grid_rows * grid_cols, sizeof(FLOAT));
@@ -296,9 +276,9 @@ int main(int argc, char **argv)
 		fatal("unable to allocate memory");
 
 	/* read initial temperatures and input power	*/
-	tfile = argv[5];
-	pfile = argv[6];
-    ofile = argv[7];
+	tfile = argv[4];
+	pfile = argv[5];
+    // ofile = argv[6];
 
 	read_input(temp, grid_rows, grid_cols, tfile);
 	read_input(power, grid_rows, grid_cols, pfile);
@@ -314,7 +294,9 @@ int main(int argc, char **argv)
     printf("Ending simulation\n");
     printf("Total time: %.3f seconds\n", ((float) (end_time - start_time)) / (1000*1000));
 
+#ifdef OUTPUT
     writeoutput((1&sim_time) ? result : temp, grid_rows, grid_cols, ofile);
+#endif
 
 	/* output results	*/
 #ifdef VERBOSE
